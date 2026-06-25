0fa75d5a5aac7382fb7be4d70ad4467be7682360
Mariano2182 <mrsoldadurasespeciales@outlook.com>
2026-06-25 13:00:16 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,49 @@
+import { loginUser } from "./auth.js";
+
+let currentUser = null;
+
+window.login = async function(){
+
+    const usuario = document.getElementById("usuario").value;
+    const password = document.getElementById("password").value;
+
+    const error = document.getElementById("error");
+
+    try{
+
+        const user = await loginUser(usuario, password);
+
+        currentUser = user;
+
+        localStorage.setItem("user", JSON.stringify(user));
+
+        document.getElementById("login-container").classList.add("hidden");
+        document.getElementById("app").classList.remove("hidden");
+
+        document.getElementById("user-info").innerText =
+            `Usuario: ${user.usuario} | Rol: ${user.rol}`;
+
+    }catch(e){
+        error.innerText = e.message;
+    }
+};
+
+window.logout = function(){
+    localStorage.removeItem("user");
+    location.reload();
+};
+
+window.onload = function(){
+
+    const saved = localStorage.getItem("user");
+
+    if(saved){
+        document.getElementById("login-container").classList.add("hidden");
+        document.getElementById("app").classList.remove("hidden");
+
+        const user = JSON.parse(saved);
+
+        document.getElementById("user-info").innerText =
+            `Usuario: ${user.usuario} | Rol: ${user.rol}`;
+    }
+};
