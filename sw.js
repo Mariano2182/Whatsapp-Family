5d5464f08dda6af7aa6adb27512470439fa33da2
Mariano2182 <mrsoldadurasespeciales@outlook.com>
2026-06-29 10:37:18 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'chat-familiar-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
