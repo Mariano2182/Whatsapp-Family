b778c245e23aa34534ad09da8020207042e30190
Mariano2182 <mrsoldadurasespeciales@outlook.com>
2026-07-21 15:13:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-import { db } from "./firebase.js";
-import { collection, addDoc, query, orderBy, onSnapshot, serverTimestamp, doc, deleteDoc, where, getDocs, updateDoc, deleteField, arrayUnion } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { loginUser, verificarYCrearUsuarioDefecto, registrarNuevoUsuario, actualizarNombreUsuario, eliminarUsuario, cambiarPasswordUsuario } from "./auth.js";
+i
 
 // 🚨 DETECTOR DE ERRORES EN PANTALLA (Ideal para ver fallas desde el celular)
 window.addEventListener('error', function(e) {
